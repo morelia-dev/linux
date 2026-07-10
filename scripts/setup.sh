d29f8399c3f57fb5aa1b96de27a85d42ca35685c
morelia <morelia-dev@protonmail.com>
2026-07-10 13:45:22 +1000

    fix: add scrot and xclip to be installed, change screenshot filenames

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,10 +6,10 @@ sudo pacman -S base-devel nano bash curl wget unzip helix rust
 
 sudo pacman -S git github-cli
 
-git config --global user.name "morelia-dev"
+git config --global user.name "morelia"
 git config --global user.email "morelia-dev@protonmail.com"
 
-sudo pacman -S i3-wm xdg-desktop-portal
+sudo pacman -S i3-wm xdg-desktop-portal scrot xclip
 
 ./install_fonts.sh
 ./install_fonts_cjk.sh
